b711143f961dd662f4b597feda96772a3deb0608
samy-blh <sbelhocine55@gmail.com>
2025-06-07 20:17:38 +0200

    Add stock management module

--- SNK_Plastic/Backend/app.js
+++ SNK_Plastic/Backend/app.js
@@ -7,12 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ Inclusion des routes
+// Inclusion des routes
 const commandesRoutes = require('./routes/commandes');
 const clientsRoutes = require('./routes/clients');
+const machinesRoutes = require('./routes/machines');
+const stocksRoutes = require('./routes/stocks');
 
 app.use('/api/commandes', commandesRoutes);
 app.use('/api/clients', clientsRoutes);
+app.use('/api/machines', machinesRoutes);
+app.use('/api/stocks', stocksRoutes);
 
 app.get('/', (req, res) => {
   res.send('API SNK Plastic opérationnelle ✔');
@@ -21,7 +25,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Serveur backend lancé sur le port ${PORT}`);
-const machinesRoutes = require('./routes/machines');
-app.use('/api/machines', machinesRoutes);
-
 });
